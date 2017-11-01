9a6957e6e6d05d28a3f2682ff20bb72f4855d7fd
Tamas Horvath <horvathtamas22@gmail.com>
2017-11-01 13:35:08 +0100

    tictac_test.py created, input functions fixed, the game asks for names, counts scores

--- tictac_test.py
+++ tictac_test.py
@@ -1,10 +1,13 @@
 import string
 
 field = []
+player_x_win = 0
+player_o_win = 0
+
 
 
 def print_field():    # print empty field with nested lists
-
+    print("\n" * 50)
 
     for i in range(field_size):
         field.append([" "])
@@ -19,8 +22,7 @@ def print_field():    # print empty field with nested lists
             line = line + str(field[i][j]) + " | "
         print(line)
         line = "| "
-
-
+    
 def move_interpreter(m):
 
     alphabet = string.ascii_lowercase
@@ -86,12 +88,19 @@ def clear_board():
 
 def load_game():
     global field_size
+    global player_x_win
+    global player_o_win
     while True:
-        field_size = int(input("Set field size(3-8): "))
-        if field_size in range(3, 9):
-            break
+        field_size = input("\nSet field size (3x3 - 8x8). Enter only one digit: ")
+        if field_size.isnumeric() == True:
+            field_size = int(field_size)
+            if field_size in range(3, 9):
+                break
+            else:
+                print("Please enter a valid value!")
+                continue
         else:
-            print("Please enter a valid value!")
+            print("Please enter a digit between 3 and 8!")
             continue
 
     print_field()
@@ -101,19 +110,25 @@ def load_game():
     while True:
         while check_empty(m) == False:
             while True:
-                m = str(input("X move?"))
-                coordinates = move_interpreter(m)
-                if coordinates[0] < field_size and coordinates[1] < field_size:
-                    break
+                m = str(input(player_x + " move? (X)"))
+                if m == "r":
+                    print("Thank you for playing. Good bye!")
+                    exit()
                 else:
-                    print("Please enter a valid field position!")
-                    continue
+                    coordinates = move_interpreter(m)
+                    if coordinates[0] < field_size and coordinates[1] < field_size:
+                        break
+                    else:
+                        print("Please enter a valid field position!")
+                        continue
 
         move("X", m)
         print_field()
         
         if win_check() == True:
-            print("X won")
+            player_x_win += 1
+            print(player_x + " won!")
+            print(player_x + ": " + str(player_x_win) + " - " + player_o + ": " + str(player_o_win))
             clear_board()
             display_menu()
         
@@ -125,19 +140,25 @@ def load_game():
 
         while check_empty(m) == False:
             while True:
-                m = str(input("O move?"))
-                coordinates = move_interpreter(m)
-                if coordinates[0] < field_size and coordinates[1] < field_size:
-                    break
+                m = str(input(player_o + " move? (O)"))
+                if m == "r":
+                    print("Thank you for playing. Good bye!")
+                    exit()
                 else:
-                    print("Please enter a valid field position!")
-                    continue
+                    coordinates = move_interpreter(m)
+                    if coordinates[0] < field_size and coordinates[1] < field_size:
+                        break
+                    else:
+                        print("Please enter a valid field position!")
+                        continue
             
         move("O", m)
         print_field()
         
         if win_check() == True:
-            print("0 won")
+            player_o_win += 1
+            print(player_o +  " won!")
+            print(player_x + ": " + str(player_x_win) + " - " + player_o + ": " + str(player_o_win))
             clear_board()
             display_menu()
         
@@ -149,19 +170,27 @@ def load_game():
         
 
 def display_menu():
-    menu_dict= {"s:":"Start game", "q:":"Quit game"}
 
     while True:
-        for key in menu_dict:
-            print(key, menu_dict[key])
+        print("\ns: Start game")
+        print("q: Quit game\n")
         user_input = str(input("Select menu item: "))
 
         if user_input == "s":
             load_game()
         elif user_input == "q":
+            print("\nThe final score is: " + player_x + ": " + str(player_x_win) + " - " + player_o + ": " + str(player_o_win))
+            print("Good bye!")
             exit()
         elif user_input == "r":
             continue
 
+def add_name():
+    global player_x
+    global player_o
+    player_x = input("Please enter Player X name: ")
+    player_o = input("Please enter Player O name: ")
+    display_menu()
 
-display_menu()
+
+add_name()
